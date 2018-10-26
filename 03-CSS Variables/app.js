527f297e4738c39bcac2bde7a8c082fd1e781079
gloriajun <pureainu@gmail.com>
2018-10-27 01:05:01 +0900

    [Modify] CSS Variables - modify by using css variables

--- 03-CSS Variables/app.js
+++ 03-CSS Variables/app.js
@@ -1,33 +1,28 @@
-const hl = document.querySelector('h2 > span.hl');
-const space = document.getElementById('spacing');
-const spaceSizing = space.getAttribute('data-sizing');
-const blur = document.getElementById('blur');
-const blurSizing = blur.getAttribute('data-sizing');
-const color = document.getElementById('base');
-const image = document.querySelector('img');
+const appView = (function () {
+  return {
+    changeStyle() {
+      const {
+        name,
+        value,
+        dataset,
+      } = this;
+      document.documentElement.style.setProperty(`--${name}`, `${value}${dataset.sizing || ''}`);
+    },
+  }
+})();
 
-function init() {
-  const hex = color.value;
-  hl.style.color = hex;
-  image.style.cssText = `
-    border: ${space.value}${spaceSizing} solid ${hex};
-    filter: blur(${blur.value}${blurSizing});
-  `;
-}
+const appController = (function () {
+  return {
+    init() {
+      // add event
+      const inputs = document.querySelectorAll('div.controls > input');
+      console.log(inputs);
+      inputs.forEach(input => input.addEventListener('change', appView.changeStyle));
+      // inputs.forEach(input => input.addEventListener('mousemove', appView.changeStyle));
+    }
+  }
+})();
 
 document.addEventListener('DOMContentLoaded', () => {
-  init();
-
-  // add event
-  space.addEventListener('change', (e) => {
-    image.style.border = `${e.target.value}${spaceSizing} solid ${color.value}`;
-  });
-  blur.addEventListener('change', (e) => {
-    image.style.filter = `blur(${e.target.value}${blurSizing})`;
-  });
-  color.addEventListener('change', (e) => {
-    const hex = e.target.value;
-    hl.style.color = hex;
-    image.style.borderColor = hex;
-  });
+  appController.init();
 });
