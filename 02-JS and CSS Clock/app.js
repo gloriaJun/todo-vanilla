ecb79c90707bb484f08a149541e959ccae3baca7
gloriajun <pureainu@gmail.com>
2018-10-26 23:44:56 +0900

    [Modify] JS and CSS Clock - refact to MVC pattern

--- 02-JS and CSS Clock/app.js
+++ 02-JS and CSS Clock/app.js
@@ -1,23 +1,35 @@
-const clock = document.querySelector('div.clock-face');
-const hourEl = clock.querySelector('div.hour-hand');
-const minEl = clock.querySelector('div.min-hand');
-const secondEl = clock.querySelector('div.second-hand');
+const appView = (function () {
+  const clock = document.querySelector('div.clock-face');
+  const hourEl = clock.querySelector('div.hour-hand');
+  const minEl = clock.querySelector('div.min-hand');
+  const secondEl = clock.querySelector('div.second-hand');
 
-function setTransform(el, degrees) {
-  el.style.transform = `rotate(${degrees + 90}deg)`;
-}
+  function setTransform(el, degrees) {
+    el.style.transform = `rotate(${degrees + 90}deg)`;
+  }
 
-function setDate() {
-  const now = new Date();
-  const hour = now.getHours();
-  const min = now.getMinutes();
-  const seconds = now.getSeconds();
+  return {
+    render() {
+      const now = new Date();
+      const hour = now.getHours();
+      const min = now.getMinutes();
+      const seconds = now.getSeconds();
 
-  setTransform(hourEl, ((hour/12) * 360));
-  setTransform(minEl, ((min/60) * 360));
-  setTransform(secondEl, ((seconds/60) * 360));
-}
+      setTransform(hourEl, ((hour/12) * 360));
+      setTransform(minEl, ((min/60) * 360));
+      setTransform(secondEl, ((seconds/60) * 360));
+    },
+  }
+})();
+
+const appController = (function () {
+  return {
+    init() {
+      setInterval(appView.render, 1000);
+    },
+  }
+})();
 
 document.addEventListener('DOMContentLoaded', () => {
-  setInterval(setDate, 1000);
+  appController.init();
 });
