b7960cabd164b28e51f64fec81ed27caa1fe7695
gloriajun <pureainu@gmail.com>
2018-10-28 19:19:18 +0900

    [Modify] Type Ahead - ver0.1

--- 06-Type Ahead/README.md
+++ 06-Type Ahead/README.md
@@ -2,4 +2,15 @@
 -
 
 ## Today I Learn
+#### Fetch API
+`Fetch API`는 Promise를 기반으로 되어있다.
+```javascript
+fetch('http://hanur.me/users')
+.then(res => res.json())
+.then(data => data.filter(item => item.isRequired));
+```
 
+- [fetch API](https://opentutorials.org/course/3281/20562)
+- [Javascript Fetch API](https://medium.com/@kkak10/javascript-fetch-api-e26bfeaad9b6)
+
+#### number format
--- 06-Type Ahead/app.js
+++ 06-Type Ahead/app.js
@@ -1,20 +1,41 @@
 const appModel = (function () {
+  const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
   return {
-    endpoint: 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json',
-    data() {
+    data: (query) => {
+      const re = new RegExp(query, 'i');
+      return fetch(endpoint)
+        .then(res => res.json())
+        .then(data => data.filter(obj => re.test(obj.city) || re.test(obj.state)));
     },
   }
 })();
 
 const appView = (function () {
+  const el = document.querySelector('ul.suggestions');
+
   return {
+    render(query, list) {
+      let html = [];
+      list.forEach(item => {
+        html.push(`<li>
+          ${item.city}, ${item.state}
+          <span class="population">${item.population}</span>
+        </li>`)
+      });
+      el.innerHTML = html.join('');
+    },
   }
 })();
 
 const appController = (function () {
   return {
     init() {
-      console.log(appModel.endpoint);
+      document.querySelector('input.search')
+        .addEventListener('input', this.search);
+    },
+    search() {
+      const { value: query } = this;
+      appModel.data(query).then(data => appView.render(query, data));
     },
   }
 })();
