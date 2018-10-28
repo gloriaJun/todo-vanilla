53a4f834ef0057871b5ffdcdac71e7aac7b2c3ba
gloriajun <pureainu@gmail.com>
2018-10-28 12:40:46 +0900

    [Modify] Flex Panel Gallery - done except animation

--- 05-Flex Panel Gallery/README.md
+++ 05-Flex Panel Gallery/README.md
@@ -1,4 +1,15 @@
 ## Exercise 5: Flex Panel Gallery
+- 이미지를 배치하고 클릭하면 애니메이션 효과와 함께 펼쳐진다.
 
 ## Today I Learn
+#### Flex
+- [A Complete Guide to Flexbox](https://css-tricks.com/snippets/css/a-guide-to-flexbox/)
+- all items to be 100% width, via flex-basis 
+```
+flex: 1 100%;
+/* `flex: 1 100%`는 아래와 같이 풀어서 표현할 수 있다. 
+flex-grow: 1;
+flex-basis: 100%;
+*/
+```
 
--- 05-Flex Panel Gallery/app.js
+++ 05-Flex Panel Gallery/app.js
@@ -1,1 +1,12 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+  const panels = document.querySelectorAll('.panel');
+  panels.forEach(panel => panel.addEventListener('click', function(e) {
+    const { classList } = this;
+    const OPEN = 'open';
+    if (classList.contains(OPEN)) {
+      classList.remove(OPEN);
+    } else {
+      classList.add(OPEN);
+    }
+  }));
+});
