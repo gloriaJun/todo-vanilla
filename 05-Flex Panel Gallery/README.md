b92a5cb25079e9dfcd43b9d84a6368e7c163a280
gloriajun <pureainu@gmail.com>
2018-10-28 13:16:18 +0900

    [Modify] Flex Panel Gallery - refactoring by mvc pattern

--- 05-Flex Panel Gallery/README.md
+++ 05-Flex Panel Gallery/README.md
@@ -13,3 +13,16 @@ flex-basis: 100%;
 */
 ```
 
+#### transform - translate 
+- 요소를 현재의 위치를 기준으로 이동시킨다.
+- [transform 속성 : 요소의 표시 위치를 이동시키는 translate](https://www.tabmode.com/homepage/transform-translate.html)
+
+
+#### toggle
+`classList.toggle()`을 이용하면 엘리먼트의 class name을 제어할 수 있다.
+```
+toggle( String [, force] )
+한개의 인수만 있을 때 : 클래스 값을 변환한다. 즉 클래스가 존재한다면 지우고 false를 반환하며, 존재하지 않다면 그것을 추가하고 true를 반환한다.
+두번째 인수가 있을 때 : 두번째 인수가 true로 평가되면 특정 클래스 값을 추가하고 false로 평가되면 지운다.
+```
+- [Element.classList](https://developer.mozilla.org/ko/docs/Web/API/Element/classList)  
--- 05-Flex Panel Gallery/app.js
+++ 05-Flex Panel Gallery/app.js
@@ -1,12 +1,28 @@
+const appView = (function () {
+  return {
+    toggleOpen() {
+      this.classList.toggle('open');
+    },
+    toggleActive(e) {
+      if (e.propertyName.includes('flex')) {
+        this.classList.toggle('open-active');
+      }
+    },
+  }
+})();
+
+const appController = (function () {
+  return {
+    init() {
+      const panels = document.querySelectorAll('.panel');
+      panels.forEach(panel => {
+        panel.addEventListener('click', appView.toggleOpen);
+        panel.addEventListener('transitionend', appView.toggleActive);
+      });
+    },
+  }
+})();
+
 document.addEventListener('DOMContentLoaded', () => {
-  const panels = document.querySelectorAll('.panel');
-  panels.forEach(panel => panel.addEventListener('click', function(e) {
-    const { classList } = this;
-    const OPEN = 'open';
-    if (classList.contains(OPEN)) {
-      classList.remove(OPEN);
-    } else {
-      classList.add(OPEN);
-    }
-  }));
+  appController.init();
 });
